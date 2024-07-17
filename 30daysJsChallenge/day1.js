374a2520aa2774b23c1ab147b893f7c62d1eceed
R-k-o29 <121087246+R-k-o29@users.noreply.github.com>
2024-07-17 09:14:23 +0530

    Update day1.js

--- 30daysJsChallenge/day1.js
+++ 30daysJsChallenge/day1.js
@@ -1,1 +1,39 @@
+// Task1 
+var num = 10;
+console.log(num);
+
+// Task2
+let str = "Hello, world!";
+console.log(str);
+
+// Task3
+const isJavaScriptFun = true;
+console.log(isJavaScriptFun);
+
+// Task4
+let number = 42;
+let string = "JavaScript";
+let boolean = false;
+let object = { name: "John", age: 30 };
+let array = [1, 2, 3, 4, 5];
+console.log(typeof number);   // number
+console.log(typeof string);   // string
+console.log(typeof boolean);  // boolean
+console.log(typeof object);   // object
+console.log(typeof array);    // object (arrays are a type of object in JavaScript)
+
+// Task5
+let initialValue = 100;
+console.log(initialValue);
+initialValue = 200;
+console.log(initialValue);
+
+// Task6
+const constantValue = 50;
+console.log(constantValue);
+// Trying to reassign the constant variable will result in an error
+// Uncommenting the following line will throw a TypeError
+// constantValue = 100; 
+
+
 
