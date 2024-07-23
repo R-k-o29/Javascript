70c87d3a10b97f16b004565c0f42a756c8d1c6d0
R-k-o29 <121087246+R-k-o29@users.noreply.github.com>
2024-07-23 22:12:13 +0530

    Update day10.js

--- 30daysJsChallenge/day10.js
+++ 30daysJsChallenge/day10.js
@@ -24,10 +24,29 @@
   <p id="inputtext"></p>
   <input type="text" name="" id="textinput" placeholder="Write here">
   <h1>task7</h1>
+  <form id="myForm">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="password" placeholder="Password">
+        <button type="submit">Submit</button>
+  </form>
+  <h1>task8</h1>
+  <p id="update"></p>
+  <select name="" id="values">
+    <option value="1">Option 1</option>
+    <option value="2">Option 2</option>
+    <option value="3">Option 3</option>
+  </select>
+  <h1>task9</h1>
+  <ul id="list">
+    <li>item1</li>
+    <li>item2</li>
+    <li>item3</li>
+  </ul>
+  <h1>task10</h1>
+  <button type="button" id="additem">Click to Add item</button>
+  <li id="dynamic">
+  </li>
   
-  
-
-
   <script src="scriptt.js"></script>
 </body>
 </html>
@@ -69,3 +88,33 @@ text.addEventListener("keyup",(e)=>{
     inputtext.textContent=`current text: ${text.value}`;
 });
 //TASK7
+const form = document.getElementById("myForm");
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // Prevents the default form submission
+  const formData = new FormData(form);
+  for (let [key, value] of formData.entries()) {
+    console.log(`${key}: ${value}`);
+  }
+});
+
+//TASK8
+const value=document.getElementById("values");
+const update=document.getElementById("update");
+value.addEventListener("change",()=>{
+    update.textContent=`selected option: ${value.value}`;
+});
+//TASK9
+const list=document.getElementById("list");
+list.addEventListener('click',(e)=>{
+    if(e.target.tagName==='LI'){
+        console.log(`clicked value : ${e.target.textContent}`);
+    }
+});
+//TASK10
+const dynamic=document.getElementById("dynamic");
+const newitem=document.getElementById("additem");
+newitem.addEventListener("click",()=>{
+    const newelement=document.createElement("LI");
+    newelement.textContent=`New item ${dynamic.children.length + 1}`;
+    dynamic.appendChild(newelement);
+})
