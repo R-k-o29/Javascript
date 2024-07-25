ad5800f571a1f930ae9487da2adea4c7567bac07
ROHIT KUMAR OJHA <121087246+R-k-o29@users.noreply.github.com>
2024-07-26 00:16:37 +0530

    Update day13.js
    MODULES

--- 30daysJsChallenge/day13.js
+++ 30daysJsChallenge/day13.js
@@ -1,4 +1,4 @@
-<h1>TASK 1</h1>
+// TASK 1
 
 //module 1 : one.js
 const add=(a,b)=>{
@@ -10,6 +10,40 @@ module.exports={add};
 //module 2 : two.js
 const mod=require("./one");
 console.log("Sum : ",mod.add(4,5));
-
-
+// -------------------------------------------------------------------------------------------------------
 //TASK 2 
+
+//module 1: one.js
+const person={
+    name : "Rohit",
+    age : 22,
+    Gender : "Male" ,
+    printName : ()=>{
+        console.log(`This is ${person.name}`);
+    },
+    printAge : ()=>{
+        console.log(`${person.name}'s age is ${person.age}`);
+    }
+}
+
+module.exports = {person};
+
+//module2: two.js
+const mod=require("./one");
+
+console.log(mod.person.name);
+mod.person.printAge();
+console.log(mod.person.Gender);
+// -------------------------------------------------------------------------------------------------------
+//TASK 3
+//module 1: one.js
+export const name = ()=>{
+    console.log("Rohit");
+};
+//module2: two.js
+import name  from "./one";
+name();
+// -------------------------------------------------------------------------------------------------------
+//TASK 4
+//module 1: one.js
+
