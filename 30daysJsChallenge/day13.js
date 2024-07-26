f8d28667ec8b668d451fac13a7e6402d15013eed
ROHIT KUMAR OJHA <121087246+R-k-o29@users.noreply.github.com>
2024-07-26 19:18:39 +0530

    Update day13.js

--- 30daysJsChallenge/day13.js
+++ 30daysJsChallenge/day13.js
@@ -54,3 +54,24 @@ import name from "./two";
 name();
 
 // -------------------------------------------------------------------------------------------------------
+//TASK 6
+var _ = require('lodash');// run in terminal ( npm i --save lodash )
+const arr=[1,2,3,4,5];
+const chuncked=_.chunk(arr,2);
+console.log(chuncked);
+
+// -------------------------------------------------------------------------------------------------------
+//TASK 7
+
+const axios = require("axios");
+
+axios
+  .get("https://jsonplaceholder.typicode.com/posts/1")
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
+// -------------------------------------------------------------------------------------------------------
+//TASK 8
