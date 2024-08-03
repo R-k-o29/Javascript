f1827b010e40a936ef829d184e008f47295433ab
ROHIT KUMAR OJHA <121087246+R-k-o29@users.noreply.github.com>
2024-08-03 17:43:51 +0530

    Update day17.js
    DATA STRUCTURES

--- 30daysJsChallenge/day17.js
+++ 30daysJsChallenge/day17.js
@@ -112,4 +112,92 @@ console.log("Top element:", stack.peek()); // Outputs: Top element: 2
 
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //TASK4
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop() {
+    if (this.items.length === 0) {
+      return "Stack is empty";
+    }
+    return this.items.pop();
+  }
+
+  peek() {
+    if (this.items.length === 0) {
+      return "Stack is empty";
+    }
+    return this.items[this.items.length - 1];
+  }
+
+  displayStack() {
+    console.log(this.items.join(" "));
+  }
+}
+
+const reverseString = (str) => {
+  const stack = new Stack();
+  for (let i = 0; i < str.length; i++) {
+    stack.push(str[i]);
+  }
+  let reversed = "";
+  while (stack.items.length > 0) {
+    reversed += stack.pop();
+  }
+  return reversed;
+};
+
+
+const originalString = "hello";
+const reversedString = reverseString(originalString);
+console.log("Original:", originalString); // Outputs: Original: hello
+console.log("Reversed:", reversedString); // Outputs: Reversed: olleh
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//TASK5
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+
+  enqueue(element) {
+    this.items.push(element);
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return "Queue is empty";
+    }
+    return this.items.shift();
+  }
+
+  front() {
+    if (this.isEmpty()) {
+      return "Queue is empty";
+    }
+    return this.items[0];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  displayQueue() {
+    console.log(this.items);
+  }
+}
+
+const queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.displayQueue(); // Outputs: [1,2,3]
+console.log("Dequeued:", queue.dequeue()); // Outputs: Dequeued: 1
+queue.displayQueue(); // Outputs: [2,3]
+console.log("Front element:", queue.front()); // Outputs: Front element: 2
 
