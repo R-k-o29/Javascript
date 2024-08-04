0efc4eda07eb76a312334567480f5d56fc4787a1
ROHIT KUMAR OJHA <121087246+R-k-o29@users.noreply.github.com>
2024-08-04 10:59:21 +0530

    Update day18.js

--- 30daysJsChallenge/day18.js
+++ 30daysJsChallenge/day18.js
@@ -18,3 +18,34 @@ let arr = [5, 4, 65, 65, 1];
 console.log(bubbleSort(arr));
 // -------------------------------------------------------------------------------------------------------------------------------------------------
 //TASK2
+const selectionSort=(arr)=>{
+    for (let i = 0; i < arr.length; i++) {
+        let last=arr.length-1-i;
+        let max=findMax(arr,0,last);
+        swap(arr,max,last);
+    }
+}
+
+const swap=(arr,i,j)=>{
+    let temp=arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;
+}
+
+const findMax=(arr,start,end)=>{
+    let max=start;
+    for (let i = 0; i <= end; i++) {
+        if(arr[max]<arr[i]){
+            max=i;
+        }
+    }
+    return max;
+}
+
+
+let arr = [5, 4, 65, 65, 1];
+selectionSort(arr)
+console.log(arr);
+// -------------------------------------------------------------------------------------------------------------------------------------------------
+//TASK3
+
